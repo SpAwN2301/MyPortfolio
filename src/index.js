545ee673a657807d8e87c8531549c5a19232936d
new-store <vadimpronkin19@gmail.com>
2021-07-12 21:40:15 +0300

    project adding automize

--- src/index.js
+++ src/index.js
@@ -4,7 +4,45 @@ const menu = body.querySelector(".menu");
 const menuItems = menu.querySelectorAll(".menu__item");
 const menuContent = document.querySelector(".content").children;
 const menuBorder = menu.querySelector(".menu__border");
+let projects = document.getElementById('projects');
 let activeItem = menu.querySelector(".active");
+
+const projectsArr = [
+    {
+        name: 'Smart Orders',
+        description: 'Сервис для быстрого оформления заказа в местах общепита и ресторанах. Всё, что нужно пользователю — это отсканировать QR код своего столика и выбрать предпочитаемые блюда из меню. Меню формируется на основе Гугл таблиц, что позволяет работникам легко и удобно управлять его содержимым. Оформленный заказ моментально попадает в базу данных и на экраны персонала, доступ к которому есть только у авторизованных работников заведения.'
+    },
+    {
+        name: 'MyPortfolio',
+        description: 'Немного рекурсии :)'
+    },
+    {
+        name: 'TestPayment',
+        description: 'Тестовое задание — Форма оформления заказа.'
+    },
+    {
+        name: 'TestCalculator',
+        description: 'Тестовое задание — Калькулятор для расчета стоимости натяжных потолков с установкой.'
+    },
+    {
+        name: 'Uber',
+        description: 'Мой первый проект'
+    },
+]
+
+projectsRender();
+
+function projectsRender(){
+    for(let i = 0; i < projectsArr.length; i++){
+        projects.innerHTML += `
+        <li class="myProject">
+            <div class="myProject__title">${projectsArr[i].name}</div>
+            <div class="myProject__descrip">${projectsArr[i].description}</div>
+            <a target="_blank" href="https://spawn2301.github.io/${projectsArr[i].name.split(' ').join('')}/" class="myProject__link">Go to Website</a>
+        </li>
+        `;
+    }
+}
 
 function clickItem(item, index) {
     menu.style.removeProperty("--timeOut");
